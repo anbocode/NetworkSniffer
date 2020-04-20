f68136642608a91779d42188f93bfc9203bde898
anbocode <anjan42@gmail.com>
2020-04-20 15:34:29 +0530

    LadingPage.go modified

--- web/LandingPage.go
+++ web/LandingPage.go
@@ -8,27 +8,34 @@ import (
 	"os"
 )
 
-func LandingPage() {
+func handleFunc01(w http.ResponseWriter, r *http.Request) {
 	var (
 		interfaces         structures.ReturnStructure = structures.ReturnStructure{}
 		unrecognisedMethod string                     = `
 			"Data": "Page broken"
 		`
-		err error = nil
 	)
 
 	interfaces = network.GetInterfaces()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-		switch r.Method {
-		case "GET":
-			fmt.Fprintf(w, string(interfaces.Result))
-		default:
-			fmt.Fprintf(w, unrecognisedMethod)
-		}
-	})
+	switch r.Method {
+	case "GET":
+		fmt.Fprintf(w, string(interfaces.Result))
+	default:
+		fmt.Fprintf(w, unrecognisedMethod)
+	}
+}
+
+func LandingPage() {
+	var (
+		fileServer http.Handler = http.FileServer(http.Dir("static/"))
+		err        error        = nil
+	)
+
+	http.HandleFunc("/interfaces", handleFunc01)
+	http.Handle("/main/", http.StripPrefix("/main/", fileServer))
 
 	if http.ListenAndServe("127.0.0.1:8989", nil); err != nil {
 		fmt.Println("Error | http.ListenAndServe() : ", err.Error())
